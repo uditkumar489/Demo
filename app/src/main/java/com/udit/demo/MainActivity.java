eef81cf34fd7f1f0280be91fbc569b7e47ed9bae
uditkumar489 <uditkumar489@gmail.com>
2017-06-21 14:11:03 +0530

    Initial commit

---
+++ app/src/main/java/com/udit/demo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.udit.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void test (View view2)
+    {
+        ImageView counter = (ImageView) findViewById(R.id.test) ;
+        counter.setTranslationY(-1200f);
+        counter.setImageResource(R.drawable.udtech);
+        counter.animate().translationY(0f).setDuration(300);
+
+    }
+
+    public void clicked(View view)
+    {
+
+        Log.i("info" , "dba diya");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
